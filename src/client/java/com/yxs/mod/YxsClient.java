df027f9f18a7ee58249df4358556380e51136969
yxs <atwyxsa02@163.com>
2025-11-22 00:33:10 +0800

    Main init

---
+++ src/client/java/com/yxs/mod/YxsClient.java
@@ -0,0 +1,10 @@
+package com.yxs.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YxsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
